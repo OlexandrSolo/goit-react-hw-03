de84c9d2b8800d1bc1422fa6a738377c49e864c1
Your Name <you@example.com>
2024-08-23 07:24:58 +0300

    creat contact form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,68 @@
-import { useState } from "react";
+import { useId, useState } from "react";
 import "./App.css";
 
+const initialValue = {
+  username: "",
+  numberPhone: "",
+};
+
 function App() {
+  const id = useId();
+  const [userDate, setUserDate] = useState(initialValue);
+
   const handleSubmitForm = (evt) => {
-    console.log(evt);
+    evt.preventDefault();
+    console.log(userDate);
+    setUserDate(initialValue);
+  };
+  const handleChange = (evt) => {
+    setUserDate({
+      ...userDate,
+      [evt.target.name]: evt.target.value,
+    });
   };
   return (
-    <form onSubmit={handleSubmitForm}>
-      <label htmlFor="username">Name</label>
-      <input type="text" name="username" />
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmitForm}>
+        <div className="username">
+          <label htmlFor={`${id}-'userName'`}>Name</label>
+          <input
+            type="text"
+            value={userDate.username}
+            onChange={handleChange}
+            name="username"
+            id={`${id}-'userName'`}
+          />
+        </div>
+        <div className="numberPhone">
+          <label htmlFor={`${id}-'numberPhone'`}>Number</label>
+          <input
+            type="text"
+            value={userDate.numberPhone}
+            onChange={handleChange}
+            name="numberPhone"
+            id={`${id}-'numberPhone'`}
+          />
+        </div>
+
+        <button type="submit">Add contact</button>
+      </form>
+
+      <div className="wrapper">
+        <label htmlFor={`${id}-'searchContact'`}>Find contacts by name</label>
+        <input type="text" name="searchContact" id={`${id}-'searchContact'`} />
+      </div>
+
+      <div>
+        <ul>
+          <li>
+            <span></span>
+            <span></span>
+            <button></button>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
 
