4d744342a941d7435a4030edd703dc7b22c2dfca
Your Name <you@example.com>
2024-08-25 18:45:09 +0300

    create a form using the library Formik

--- src/component/ContactForm/ContactForm.jsx
+++ src/component/ContactForm/ContactForm.jsx
@@ -1,33 +1,43 @@
 import { nanoid } from "nanoid";
 import { useId } from "react";
 import PropTypes from "prop-types";
+import { Form, Field, Formik } from "formik";
 
 export default function ContactForm({ updateContactList }) {
   const idLabel = useId();
-  const handleSubmitForm = (evt) => {
-    evt.preventDefault();
+
+  const handleSubmitForm = (values, actions) => {
     const id = nanoid();
+
     updateContactList({
       id,
-      name: evt.target.name.value,
-      number: evt.target.number.value,
+      name: values.name,
+      number: values.number,
     });
-    evt.target.reset();
+
+    actions.resetForm();
   };
 
   return (
-    <form onSubmit={handleSubmitForm}>
-      <div className="userName">
-        <label htmlFor={`${idLabel}-'name'`}>Name</label>
-        <input type="text" name="name" id={`${idLabel}-'name'`} />
-      </div>
-      <div className="number">
-        <label htmlFor={`${idLabel}-'number'`}>Number</label>
-        <input type="text" name="number" id={`${idLabel}-'number'`} />
-      </div>
-
-      <button type="submit">Add contact</button>
-    </form>
+    <Formik
+      onSubmit={handleSubmitForm}
+      initialValues={{
+        name: "",
+        number: "",
+      }}
+    >
+      <Form>
+        <div className="group">
+          <label htmlFor={`${idLabel}-'name'`}>Name</label>
+          <Field type="text" name="name" id={`${idLabel}-'name'`} />
+        </div>
+        <div className="group">
+          <label htmlFor={`${idLabel}-'number'`}>Number</label>
+          <Field type="number" name="number" id={`${idLabel}-'number'`} />
+        </div>
+        <button type="submit">Add contact</button>
+      </Form>
+    </Formik>
   );
 }
 
